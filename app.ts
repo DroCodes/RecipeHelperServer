653dfe72e369f96b14d68fc3e106f8925562cc12
DroCodes <101131393+DroCodes@users.noreply.github.com>
2022-12-13 12:21:46 -0600

    edited app.ts

--- app.ts
+++ app.ts
@@ -3,11 +3,11 @@ import multer from "multer";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { main } from "./config/server";
-// import router from "./api-routes/TestRoute";
 import { RecipeRouter } from "./api-routes/RecipeRoute";
 import { imageRouter } from "./api-routes/recipeImageRoute";
 
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -17,8 +17,8 @@ app.use("/static", express.static("uploads"));
 app.use("/recipes", RecipeRouter);
 app.use("/uploadFile", imageRouter);
 
-app.listen(process.env.PORT || 8080, () => {
-    console.log("Server started on port 8080");
+app.listen(port, () => {
+    console.log("Server started on port " + port + ".");
     main().catch((err) => {
         console.log(err);
     });
